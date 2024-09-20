8576ffc58356dfeea5d0e647e22fd475c3666570
Oussama Atmani <borntolearn314@gmail.com>
2024-09-20 17:22:32 +0100

    web

---
+++ go.mod
@@ -0,0 +1,3 @@
+module webb
+
+go 1.22.3
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+	"webb/ascii"
+)
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		fmt.Fprintln(w, "error hh 404")
+		return
+	}
+	temp, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	temp.Execute(w, err)
+}
+
+func Ascii(w http.ResponseWriter, r *http.Request) {
+	input := r.FormValue("text")
+	banner := r.FormValue("banner")
+	Print := ascii.PrintArt(input, banner)
+	temp, err := template.ParseFiles("templates/index.html")
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	temp.Execute(w, Print)
+}
+
+func main() {
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/ascii", Ascii)
+	log.Println("http://localhost:8080")
+	http.ListenAndServe(":8080", nil)
+}
